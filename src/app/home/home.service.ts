54bf3a7ac580a1a8357e2646fabe363fa2758c9f
pavan6717624 <pvn.kumar4@gmail.com>
2023-12-08 11:18:28 +0530

    deploy

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -6,43 +6,43 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeService {
   editContent(formData: FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/editContent',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/editContent',formData);
   }
 
   changeTemplate(formData: FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/changeTemplate',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/changeTemplate',formData);
   }
 
   downloadImage(formData:FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/downloadImage',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/downloadImage',formData);
   }
 
   postToFacebookImage(formData:FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/postToFacebookImage',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/postToFacebookImage',formData);
   }
 
   postToFacebookVideo(formData:FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/postToFacebookVideo',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/postToFacebookVideo',formData);
   }
 
   
   getTemplate(formData:FormData) {
-    return this.http.post('https://takeoff-pavan.herokuapp.com/Heidigi/getTemplate',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/getTemplate',formData);
   }
 
   constructor(private http: HttpClient) { }
   getImages()
   {
-    return this.http.get('https://takeoff-pavan.herokuapp.com/Heidigi/getImages');
+    return this.http.get('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/getImages');
   }
 
   getVideos()
   {
-    return this.http.get('https://takeoff-pavan.herokuapp.com/Heidigi/getVideos');
+    return this.http.get('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/getVideos');
   }
 
   getProfile()
   {
-    return this.http.get('https://takeoff-pavan.herokuapp.com/Heidigi/getProfile');
+    return this.http.get('https://heidigi-app-38b2318c83b0.herokuapp.com//Heidigi/getProfile');
   }
 }
