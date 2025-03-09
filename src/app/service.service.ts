fbe30a35af556631cb80c729112b5d191167d7a5
Pavan <pvn.kumar4@gmail.com>
2025-03-09 14:44:24 +0530

    demo

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -6,36 +6,36 @@ import { Login, Signup } from './login/login.component';
 })
 export class ServiceService {
   getOrderId() {
-    return this.http.get( 'http://localhost:8081/JOLLY/getOrderId' );
+    return this.http.get( 'https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/getOrderId' );
   }
 
   sendOTP(formData: FormData) {
-    return this.http.post( 'http://localhost:8081/JOLLY/sendOTP',formData );
+    return this.http.post( 'https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/sendOTP',formData );
   }
 
   verifyOTP(formData: FormData) {
-    return this.http.post( 'http://localhost:8081/JOLLY/verifyOTP',formData );
+    return this.http.post( 'https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/verifyOTP',formData );
   }
 
   signin(signUp: Signup) {
-    return this.http.post( 'http://localhost:8081/JOLLY/signup',signUp );
+    return this.http.post( 'https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/signup',signUp );
   }
 
   constructor(private http: HttpClient) { }
-  
+
 
   login(login: Login)
   {
-    return this.http.post('http://localhost:8081/JOLLY/login',login);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/login',login);
   }
 
   generateOTP(formData: FormData)
   {
-    return this.http.post('http://localhost:8081/JOLLY/generateOTP',formData);
+    return this.http.post('https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/generateOTP',formData);
   }
 
   getLoginDetails() {
-    return this.http.get('http://localhost:8081/JOLLY/getLoginDetails')
+    return this.http.get('https://heidigi-app-38b2318c83b0.herokuapp.com/JOLLY/getLoginDetails')
   }
 
   
