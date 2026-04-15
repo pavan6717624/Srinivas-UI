8f40be104e4cd5788e8611ee6f2bb0f48763b2e9
Pavan <pvn.kumar4@gmail.com>
2026-04-15 11:41:17 +0530

    add

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -9,107 +9,107 @@ import { CustomerDTO } from './home/admin/customer/customer.component';
 })
 export class ServiceService {
   getOrderId() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getOrderId' );
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getOrderId' );
   }
 
   sendOTP(formData: FormData) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/sendOTP',formData );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/sendOTP',formData );
   }
 
   verifyOTP(formData: FormData) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/verifyOTP',formData );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/verifyOTP',formData );
   }
 
   removeFromTrip(formData: FormData) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/removeFromTrip',formData );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/removeFromTrip',formData );
   }
 
   signin(signUp: Signup) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/signup',signUp );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/signup',signUp );
   }
 
   addLocation(locationDTO: LocationDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/addLocation',locationDTO );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/addLocation',locationDTO );
   }
 
   addTrip(tripDTO: TripDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/addTrip',tripDTO );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/addTrip',tripDTO );
   }
 
   deleteTrip(tripDTO: TripDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/deleteTrip',tripDTO );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/deleteTrip',tripDTO );
   }
 
   addCustomer(cdto: CustomerDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/addCustomer',cdto );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/addCustomer',cdto );
   }
 
   addSchedule(sdto: ScheduleDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/addSchedule',sdto );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/addSchedule',sdto );
   }
 
 
   editCustomer(cdto: CustomerDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/editCustomer',cdto );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/editCustomer',cdto );
   }
 
   deleteCustomer(cdto: CustomerDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/deleteCustomer',cdto );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/deleteCustomer',cdto );
   }
 
 
  editLocation(locationDTO: LocationDTO) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/editLocation',locationDTO );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/editLocation',locationDTO );
   }
 
  deleteLocation(formData: FormData) {
-    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/deleteLocation',formData );
+    return this.http.post( 'https://pavan-jolly-418fae2030c9.herokuapp.com/deleteLocation',formData );
   }
 
   getLocations() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getLocations');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getLocations');
   }
 
   getSchedules() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getSchedules');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getSchedules');
   }
 
   getTrips() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getTrips');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getTrips');
   }
 
   getCustomerTrips() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getCustomerTrips');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getCustomerTrips');
   }
 
   getCustomers() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getCustomers');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getCustomers');
   }
 
 
   getLocationDropDown() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getLocationDropDown');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getLocationDropDown');
   }
 
   
   getCustomersDropDown() {
-    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getCustomersDropDown');
+    return this.http.get( 'https://pavan-jolly-418fae2030c9.herokuapp.com/getCustomersDropDown');
   }
   constructor(private http: HttpClient) { }
 
 
   login(login: Login)
   {
-    return this.http.post('https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/login',login);
+    return this.http.post('https://pavan-jolly-418fae2030c9.herokuapp.com/login',login);
   }
 
   generateOTP(formData: FormData)
   {
-    return this.http.post('https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/generateOTP',formData);
+    return this.http.post('https://pavan-jolly-418fae2030c9.herokuapp.com/generateOTP',formData);
   }
 
   getLoginDetails() {
-    return this.http.get('https://pavan-jolly-418fae2030c9.herokuapp.com/JOLLY/getLoginDetails')
+    return this.http.get('https://pavan-jolly-418fae2030c9.herokuapp.com/getLoginDetails')
   }
 
   
